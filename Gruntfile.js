081e9468d411802327d6b9cc20027bbc814b7d90
Steven <admin@stevenhsu.ca>
2014-02-12 21:27:44 -0800

    Tweak Grunt Build File

--- Gruntfile.js
+++ Gruntfile.js
@@ -221,7 +221,7 @@ module.exports = function (grunt) {
     buildcontrol: {
       dist: {
         options: {
-          remote: 'origin',
+          remote: '../',
           branch: 'gh-pages',
           commit: true,
           push: true
